36ddbf73b23957bd5f9f7e42506248908d9fa2be
CrackersCookie <43343570+CrackersCookie@users.noreply.github.com>
2019-08-23 11:12:08 +0100

    Changing to api (#6)
    * making initial changes and commenting out to start testing and building on Heroku
    
    * check base display and build
    
    * commenting out more files for base
    
    * eidting fiels back further
    
    * removing firebase_admin
    
    * small changes to test Heroku
    
    * adding buildpack
    
    * adding in firebase_admin and initialised
    
    * testing firebase key
    
    * testing pyrebase config
    
    * change to pyrebase config
    
    * checking secret key
    
    * resolve issue with pyrebase version and request - working on Heroku with pyrebase and admin SDK
    
    * refactor auth to work locally for user sign ups
    
    * checking still works on Heroku
    
    * checking still works on Heroku small edit
    
    * testing still loads with login as before
    
    * working login post request and holding message added for get request
    
    * test signup with Heroku
    
    * test signup with Heroku - taking out items to find issue
    
    * test signup with Heroku - taking out items to find issue - small change
    
    * adding POST for signup back in
    
    * adding POST for signup back in with auth.create_user
    
    * changing layout and adding except in
    
    * add in auth and add user to Firestore with unique ID
    
    * add verify to login
    
    * add verify to login with small edit
    
    * add business sign up
    
    * add business login
    
    * add business login to Heroku test
    
    * change output format of logged in
    
    * change output format of logged in - small tweak for HEroku
    
    * format signup response for user
    
    * format signup response for business
    
    * checking for error
    
    * checking for error business signup
    
    * business login refactor to correct format
    
    * add enpoints.json for front end to start working with
    
    * editing for Heroku

--- app.py
+++ app.py
@@ -2,56 +2,85 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import auth
-from flask import *
+from flask import jsonify, Flask, request, render_template
+import json
+import os
 from pyrebase import pyrebase
-import secret
+# import secret
 
 app = Flask(__name__)
 
-
-# @app.route('/')
-# def index():
-#     return jsonify({"message": "messgae goes here"})
+SECRET_KEY = os.getenv("FIREBASE_API_KEY")
 
 
 # Auth -  getting user idToken
 config = {
-    "apiKey": secret.SECRET_KEY,
+    "apiKey": SECRET_KEY,
     "authDomain": "flask-auth-84403.firebaseapp.com",
     "databaseURL": "https://flask-auth-84403.firebaseio.com/",
     "storageBucket": "flask-auth-84403.appspot.com",
-    "projectId": "flask-auth-84403"
-
+    "projectId": "flask-auth-84403",
 }
 
 firebase = pyrebase.initialize_app(config)
+
 pyreAuth = firebase.auth()
 
 
-@app.route('/user/login', methods=['GET', 'POST'])
+@app.route('/api/')
+def index():
+    with open("endpoints.json", "r") as endpoints_file:
+        return json.load(endpoints_file)
+
+
+@app.route('/api/user/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        data = request.get_json(force=True)
+        email = data['email']
+        password = data['password']
         checkauth = pyreAuth.sign_in_with_email_and_password(email, password)
+        print(checkauth)
         id_token = checkauth['idToken']
+        localId = checkauth['localId']
+        display_name = checkauth['displayName']
         verify(id_token)
         if verify(id_token) == "user":
-            return redirect('/user/profile')
-    return render_template('login.html')
+            details = {}
+            details["email"] = email
+            details["display_name"] = display_name
+            details["localId"] = localId
+            user = {}
+            user["user"] = details
+            return jsonify(user)
+        return jsonify({"message": "not valid"})
+    if request.method == 'GET':
+        return jsonify({"message": "please post a user tot his endpoint"})
 
 
-@app.route('/business/login', methods=['GET', 'POST'])
+@app.route('/api/business/login', methods=['GET', 'POST'])
 def businesslogin():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        data = request.get_json(force=True)
+        email = data['email']
+        password = data['password']
         checkauth = pyreAuth.sign_in_with_email_and_password(email, password)
         id_token = checkauth['idToken']
+        id_token = checkauth['idToken']
+        localId = checkauth['localId']
+        display_name = checkauth['displayName']
         verify(id_token)
         if verify(id_token) == "business":
-            return redirect('/business/profile')
-    return render_template('businessLogin.html')
+            details = {}
+            details["email"] = email
+            details["display_name"] = display_name
+            details["localId"] = localId
+            business = {}
+            business["business"] = details
+            return jsonify(business)
+        return jsonify({"message": "not valid"})
+    if request.method == 'GET':
+        return jsonify({"message": "please post a user tot his endpoint"})
 
 
 def verify(id_token):
@@ -74,99 +103,122 @@ def verify(id_token):
             return "business"
 
 
-@app.route('/user/profile', methods=['GET', 'POST'])
-def userprofile():
-    email = "manloengchung@googlemail.com"
-    user = auth.get_user_by_email(email)
-    # print('Successfully fetched user data: {0}'.format(user.uid))
-    return render_template('loggedIn.html')
+# @app.route('/user/profile', methods=['GET', 'POST'])
+# def userprofile():
+#     email = "manloengchung@googlemail.com"
+#     user = auth.get_user_by_email(email)
+#     # print('Successfully fetched user data: {0}'.format(user.uid))
+#     return render_template('loggedIn.html')
 
 
-@app.route('/business/profile', methods=['GET', 'POST'])
-def businessprofile():
-    email = "manloengchung@googlemail.com"
-    user = auth.get_user_by_email(email)
-    # print('Successfully fetched user data: {0}'.format(user.uid))
-    return render_template('loggedIn.html')
+# @app.route('/business/profile', methods=['GET', 'POST'])
+# def businessprofile():
+#     email = "manloengchung@googlemail.com"
+#     user = auth.get_user_by_email(email)
+#     # print('Successfully fetched user data: {0}'.format(user.uid))
+#     return render_template('loggedIn.html')
 
 
-# # Admin SDK - setting up for admin privileges
-cred = credentials.Certificate("firebase-private-key.json")
-default_app = firebase_admin.initialize_app(cred)
-# print(default_app)
+# # # Admin SDK - setting up for admin privileges
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "firebase-private-key.json"
+
+default_app = firebase_admin.initialize_app()
 db = firestore.client()
 
 
-# # adds user
-@app.route('/user/signup', methods=['GET', 'POST'])
+# # # adds user
+@app.route('/api/user/signup', methods=['GET', 'POST'])
 def usersignup():
     # need to use dynamic information from the frontend submission
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        data = request.get_json(force=True)
+        email = data['email']
+        password = data['password']
+        display_name = data['display_name']
+        print(email)
+        print(password)
         # could add name from req form
         try:
             # creates a new user in firebase (under the hood)
             user = auth.create_user(
                 email=email,
                 password=password,
-                # display_name='John Doe',
+                display_name=display_name,
             )
-            # then logs in
+    #             # then logs in
             checkauth = pyreAuth.sign_in_with_email_and_password(
                 email, password)
-            # print(checkauth, "<-----")
+            print(checkauth, "<-----")
             localId = checkauth['localId']
-            # adds data into our data when user signs up and set up its own user obj
-            # needs to be more accept a range of data
+    # #             # adds data into our data when user signs up and set up its own user obj
+    # #             # needs to be more accept a range of data
             doc_ref = db.collection(u'users').document(localId)
-            doc_ref.set({u'email': email, u'name': '',
-                         u'avatar_url': 'https://placekitten.com/474/821'})
-        except:
-            # should print firebase error
-            return jsonify({'messsage': "error"})
-    return render_template('signup.html')
+            doc_ref.set({u'email': email, u'name': display_name})
+        except Exception as e:
+            print(e)
+            return jsonify("an error has occured")
+        details = {}
+        details["email"] = email
+        details["display_name"] = display_name
+        details["localId"] = localId
+        user = {}
+        user["user"] = details
+        return jsonify(user)
+    if request.method == 'GET':
+        return jsonify({"message": "please post a user tot his endpoint"})
 
 
-# # adding business
-@app.route('/business/signup', methods=['GET', 'POST'])
+# # # adding business
+@app.route('/api/business/signup', methods=['GET', 'POST'])
 def businesssignup():
-    # need to use dynamic information from the frontend submission
+    #     # need to use dynamic information from the frontend submission
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        name = request.form['name']
+        data = request.get_json(force=True)
+        email = data['email']
+        password = data['password']
+        display_name = data['display_name']
+        print(email)
+        print(password)
         try:
             # creates a new user in firebase (under the hood)
             user = auth.create_user(
                 email=email,
                 password=password,
-                display_name=name,
+                display_name=display_name,
             )
-            # then logs in
+    #             # then logs in
             checkauth = pyreAuth.sign_in_with_email_and_password(
                 email, password)
-            # print(checkauth, "<-----")
+            print(checkauth, "<-----")
             localId = checkauth['localId']
-            # adds data into our data when user signs up and set up its own user obj
-            # needs to be more accept a range of data
+        # adds data into our data when user signs up and set up its own user obj
+        # needs to be more accept a range of data
             doc_ref = db.collection(u'business').document(localId)
-            doc_ref.set({u'email': email, u'name': name})
+            doc_ref.set({u'email': email,
+                         u'display_name': display_name})
         except:
             # should print firebase error
             return jsonify({'messsage': "error"})
-    return render_template('businessSignup.html')
+        details = {}
+        details["email"] = email
+        details["display_name"] = display_name
+        details["localId"] = localId
+        business = {}
+        business["business"] = details
+        return jsonify(business)
+    if request.method == 'GET':
+        return jsonify({"message": "please post a user tot his endpoint"})
 
 
 # fetches data from db with a where clause
-@app.route('/', methods=['GET'])
-def user_data():
-    users_ref = db.collection(u'users')
-    docs = users_ref.stream()
+# @app.route('/', methods=['GET'])
+# def user_data():
+#     users_ref = db.collection(u'users')
+#     docs = users_ref.stream()
 
-    for doc in docs:
-        print(u'{} => {}'.format(doc.id, doc.to_dict()))
-        return jsonify(doc.id, doc.to_dict())
+#     for doc in docs:
+#         print(u'{} => {}'.format(doc.id, doc.to_dict()))
+#         return jsonify(doc.id, doc.to_dict())
 
 
 if __name__ == '__main__':
--- secret.py
+++ secret.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
-import os
-from pathlib import Path  # python3 only
-load_dotenv()
+# from dotenv import load_dotenv
+# import os
+# from pathlib import Path  # python3 only
+# load_dotenv()
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+# env_path = Path('.') / '.env'
+# load_dotenv(dotenv_path=env_path)
 
 
-SECRET_KEY = os.getenv("FIREBASE_API_KEY")
+# SECRET_KEY = os.getenv("FIREBASE_API_KEY")
